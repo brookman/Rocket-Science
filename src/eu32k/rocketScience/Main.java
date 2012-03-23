99367a43ca83bf27ce4f81b8b882c94e962d89e7
Benji Bryce Brookman <rc-p90@bluemail.ch>
2012-03-23 15:43:26 +0100

    test

---
+++ src/eu32k/rocketScience/Main.java
@@ -0,0 +1,13 @@
+package eu32k.rocketScience;
+
+public class Main {
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+
+   }
+
+}
